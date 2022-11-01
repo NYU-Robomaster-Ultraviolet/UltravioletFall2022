c784f77b35bb0dbafdef0f919064f90ff4c7c9dc
Arpan Pallar <arpan12pallar@gmail.com>
2022-10-31 21:01:02 -0400

    Revert "added shooter subsystem"
    This reverts commit 611d0a032980593ee08eea7f0afaa4e66cd986e3.

--- template-project/src/controls/standard/standard_control.cpp
+++ template-project/src/controls/standard/standard_control.cpp
@@ -1,77 +1,81 @@
-#include "control_interface.hpp"
+#ifdef TARGET_STANDARD
+#include "drivers.hpp"
+#include "drivers_singleton.hpp"
 
-#include "tap/architecture/clock.hpp"
-#include "tap/communication/serial/remote.hpp"
-#include "tap/drivers.hpp"
+#include "tap/control/command_mapper.hpp"
+#include "tap/control/hold_command_mapping.hpp"
+#include "tap/control/hold_repeat_command_mapping.hpp"
+#include "tap/control/press_command_mapping.hpp"
+#include "tap/control/setpoint/commands/calibrate_command.hpp"
+#include "tap/control/toggle_command_mapping.hpp"
 
+#include "subsystems/chassis/chassis_subsystem.hpp"
+#include "subsystems/gimbal/gimbal_subsystem.hpp"
+
+#include "subsystems/chassis/chassis_movement_command.hpp"
+#include "subsystems/gimbal/gimbal_movement_command.hpp"
+
+
+src::driversFunc drivers = src::DoNotUse_getDrivers;
+
+using namespace tap;
+using namespace tap::control;
 using namespace tap::communication::serial;
-/**
- * @brief Gets the current X input from the operator.
- * As the remote only returns user input every 17ms, analog input is interpolated to get a smoother usable value.
- *
- * @return float The current X input from the operator.
- */
+using namespace chassis;
+using namespace gimbal;
+
 namespace src::control{
-    float ControlInterface::getChassisXInput() {
-    uint32_t updateCounter = drivers->remote.getUpdateCounter();
-    uint32_t currTime = tap::arch::clock::getTimeMilliseconds();
+// Define subsystems here ------------------------------------------------
+ChassisSubsystem chassis(drivers());
+GimbalSubsystem gimbal(drivers());
+// Robot Specific Controllers ------------------------------------------------
 
-    if (prevUpdateCounterX != updateCounter) {
-        chassisXInput.update(drivers->remote.getChannel(Remote::Channel::RIGHT_HORIZONTAL), currTime);
-        prevUpdateCounterX = updateCounter;
-    }
+// Define commands here ---------------------------------------------------
+ChassisMovementCommand chassisMovement(&chassis, drivers());
+GimbalMovementCommand gimbalMovement(&gimbal, drivers());
+// Define command mappings here -------------------------------------------
+HoldCommandMapping rightSwitchMid(drivers(), {&chassisMovement, &gimbalMovement}, 
+RemoteMapState(Remote::Switch::RIGHT_SWITCH, Remote::SwitchState::MID));
 
-    //float finalX = limitVal<float>((chassisXInput.getInterpolatedValue(currTime)) * X_SENSITIVITY, -X_SENSITIVITY, X_SENSITIVITY);
-    float finalX = limitVal<float>((chassisXInput.getInterpolatedValue(currTime)), -1, 1);
-
-    return finalX;
-    }
-    /**
-     * @brief Gets the current Y input from the operator.
-     * As the remote only returns user input every 17ms, analog input is interpolated to get a smoother usable value.
-     *
-     * @return float The current Y input from the operator.
-     */
-    float ControlInterface::getChassisYInput() {
-        uint32_t updateCounter = drivers->remote.getUpdateCounter();
-        uint32_t currTime = tap::arch::clock::getTimeMilliseconds();
-
-        if (prevUpdateCounterY != updateCounter) {
-            chassisYInput.update(drivers->remote.getChannel(Remote::Channel::RIGHT_VERTICAL), currTime);
-            prevUpdateCounterY = updateCounter;
-        }
-
-        //float finalY = limitVal<float>((chassisYInput.getInterpolatedValue(currTime))*Y_SENSITIVITY, -Y_SENSITIVITY, Y_SENSITIVITY);
-        float finalY = limitVal<float>((chassisYInput.getInterpolatedValue(currTime)), -1, 1);
-
-        return finalY;
-    }
-    /**
-     * @brief Gets the current rotation input from the operator.
-     * As the remote only returns user input every 17ms, analog input is interpolated to get a smoother usable value.
-     *
-     * @return float The current rotation input from the operator.
-     */
-    float ControlInterface::getChassisRotationInput() {
-        uint32_t updateCounter = drivers->remote.getUpdateCounter();
-        uint32_t currTime = tap::arch::clock::getTimeMilliseconds();
-
-        if (prevUpdateCounterRotation != updateCounter) {
-            chassisRotationInput.update(drivers->remote.getChannel(Remote::Channel::LEFT_HORIZONTAL), currTime);
-            prevUpdateCounterRotation = updateCounter;
-        }
-
-        //float finalRotation = limitVal<float>((chassisRotationInput.getInterpolatedValue(currTime)) * Y_SENSITIVITY, -Y_SENSITIVITY, Y_SENSITIVITY);
-        float finalRotation = limitVal<float>((chassisRotationInput.getInterpolatedValue(currTime)), -1, 1);
-
-        return finalRotation;
-    }
-
-    float ControlInterface::getGimbalYawInput() {
-        return limitVal<float>(-drivers->remote.getChannel(Remote::Channel::LEFT_HORIZONTAL), -1, 1);
-    }
-
-    float ControlInterface::getGimbalPitchInput() {
-        return limitVal<float>(drivers->remote.getChannel(Remote::Channel::LEFT_VERTICAL), -1, 1);
-    }
+HoldCommandMapping rightSwitchUp(drivers(), {&gimbalMovement}, 
+RemoteMapState(Remote::Switch::RIGHT_SWITCH, Remote::SwitchState::UP));
+// Register subsystems here -----------------------------------------------
+void registerSubsystems(src::Drivers *drivers){
+    drivers->commandScheduler.registerSubsystem(&chassis);
+    drivers->commandScheduler.registerSubsystem(&gimbal);
 }
+// Initialize subsystems here ---------------------------------------------
+void initializeSubsystems() {
+    chassis.initialize();
+    gimbal.initialize();
+}
+// Set default command here -----------------------------------------------
+void setDefaultCommands(src::Drivers* drivers) {
+   // chassis.setDefaultCommand(&chassisMovement);
+}
+// Set Commands scheduled on startup
+void startupCommands(src::Drivers* drivers) {
+
+}
+// Register IO mappings here -----------------------------------------------
+void registerIOMappings(src::Drivers* drivers) {
+    drivers->commandMapper.addMap(&rightSwitchMid);
+    drivers->commandMapper.addMap(&rightSwitchUp);
+}
+}//namespace src::control
+
+
+// Initialize subsystems ---------------------------------------------------
+namespace src::control
+{
+    void initializeSubsystemCommands(src::Drivers* drivers)
+    {
+        initializeSubsystems();
+        registerSubsystems(drivers);
+        setDefaultCommands(drivers);
+        startupCommands(drivers);
+        registerIOMappings(drivers);
+    }
+} //namespace src::Control
+
+#endif
